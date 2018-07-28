fa605b49e205b4cb4f9f59b4c68ee720ac5c4bc9
Josh Buckner <39353526+joshbuckner@users.noreply.github.com>
2018-07-28 11:18:57 -0400

    added comments to app.js, enabled multi word search with errors

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Navigation from './components/Navigation/Navigation';
 import FilterBy from './components/Filterby/Filterby';
 import Tracklist from './components/Tracklist/Tracklist';
 import { genreList } from './components/Tracklist/sample_genre_database';
-// import PlaylistInput from './components/PlaylistInput/PlaylistInput';
-
-// 9aa1fc52decc4038a023620ce8e2f47b
 
 class App extends Component {
   constructor() {
@@ -17,77 +14,58 @@ class App extends Component {
       accessToken: ''
   	}
   }
-
-
+  // on initial page open request a new spotify api access token from /auth endpoint from backend server
   componentDidMount() {
-
     fetch('http://localhost:8888/auth')
-        .then(response => response.json())
-        .then(data => this.setState({ accessToken: data.access_token }));
+      .then(response => response.json())
+      .then(data => this.setState({ accessToken: data.access_token }));
   }
-  // componentDidMount() {
-  // 	fetch('https://api.spotify.com/v1/search?q=genre%3Apop&type=track')
-	 //  	.then(response=> {
-	 //  		response.json();
-	 //  	})
-	 //  	.then(tracks => {
-	 //  		this.setState({ genreList: tracks })
-	 //  	})
-	 //  	console.log(this.state.genreList);
-  // }
-
-
+  // update the genreList state with a json object from spotify's api based on the query the user has entered
   updateGenreList = (event) => {
-
-    // console.log(this.state.accessToken);
-      // console.log('this.state', this.state);
-      const BASE_URL = 'https://api.spotify.com/v1/search?';
-      const FETCH_URL = BASE_URL + 'query=genre%3A' + this.state.searchField + '&type=artist&market=US&offset=0&limit=50';
-      // var accessToken = ''
-
-      var myOptions = {
-        method: 'GET',
-        headers: {
-          'Authorization': 'Bearer ' + this.state.accessToken
-        },
-        mode: 'cors',
-        cache: 'default'
-      };
-
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = BASE_URL + 'query=genre%3A' + this.state.searchField + '&type=artist&market=US&offset=0&limit=50';
+    const EMPTY_FETCH_URL = BASE_URL + 'query=genre%3Aabc&type=artist&market=US&offset=0&limit=50';
+    var myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + this.state.accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+    if (this.state.searchField === "") {
+      fetch(EMPTY_FETCH_URL, myOptions)
+      .then(response => response.json())
+      .then(tracks => {        
+        this.setState({ genreList: tracks })
+      })
+    } else {
       fetch(FETCH_URL, myOptions)
-        .then(response => response.json())
-        .then(tracks => {        
-          this.setState({ genreList: tracks })
-        })
-
+      .then(response => response.json())
+      .then(tracks => {        
+        this.setState({ genreList: tracks })
+      })
     }
-
-  
-
-  onSearchChange = (event) => {
-  	this.setState({ searchField: event.target.value })
-  	// console.log(this.state.searchField);
-  	// const filteredByGenre = this.state.genreList
   }
-
-  // updateGenreList = (event) => {
-  // 	console.log(this.state.genreList);
-  // }
+  // update searchField state to reflect whatever the user has typed in
+  onSearchChange = (event) => {
+  // if searchField includes spaces add " to the beginning and end of the searchfield string, per spotify api query requirements
+    if (/\s/g.test(event.target.value)) {
+      this.setState({ searchField: '"' + event.target.value + '"' })
+    } else {
+      this.setState({ searchField: event.target.value })
+    }
+  }
 
   render() {
-  	if (this.state.genreList.length === 0) {
-  		return <h1>Loading</h1>
-  	} else {
-	    return (
-	      <div id="content" className="App">
-	        <Navigation />
-	       {/*<PlaylistInput />*/}
-	        <FilterBy updateGenreList={this.updateGenreList} searchChange={this.onSearchChange}/>
-	        {<Tracklist genreList={this.state.genreList} />}
-	      </div>
-	    );
-	  }
-  }
+    return (
+      <div id="content" className="App">
+        <Navigation />
+        <FilterBy updateGenreList={this.updateGenreList} searchChange={this.onSearchChange}/>
+        <Tracklist genreList={this.state.genreList} />
+      </div>
+    );
+	}
 }
 
 export default App;
